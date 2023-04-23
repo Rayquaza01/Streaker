e84410f7339cf25007b464bcd47dff87f436175e
Joe Jarvis <joe.jarvis109@gmail.com>
2023-04-22 22:52:14 -0400

    Remove unneeded code from list

--- src/UI/StreakList.tsx
+++ src/UI/StreakList.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import { Database } from "../Database";
@@ -13,13 +10,6 @@ export function StreakList() {
     const entries = useLiveQuery(() => Database.streaks.toArray());
 
     return (
-        // <List component="div">
-        //     {entries?.map(item => (
-        //         <ListItem key={item.id}>
-        //             <StreakCard entry={item} />
-        //         </ListItem>
-        //     ))}
-        // </List>
         <Grid container spacing={2} justifyContent="center">
             {entries?.map(item => (
                 <Grid item flexGrow={1} key={item.id}>
