d202ed89206fc312531207c99860ca46f6276d1a
Joe Jarvis <joe.jarvis109@gmail.com>
2023-04-22 22:27:18 -0400

    Start database

---
+++ src/Database.ts
@@ -0,0 +1,34 @@
+import Dexie from "dexie";
+
+export interface StreakEntry {
+    /** The id for the current entry */
+    id: number
+    /** The name of the streak */
+    name: string
+    /** The longest this streak went for */
+    longestStreak: number
+    /** The current length of the streak */
+    currentStreak: number
+    /** The date the streak was last updated */
+    lastUpdated: Date
+}
+
+class StreakDatabase extends Dexie {
+    streaks!: Dexie.Table<StreakEntry, number>;
+
+    constructor() {
+        super("StreakDatabase");
+        this.version(1).stores({
+            streaks: "++id"
+        });
+    }
+}
+
+export const Database = new StreakDatabase();
+
+Database.streaks.bulkPut([
+    { id: 1, name: "Streak 1", longestStreak: 0, currentStreak: 0, lastUpdated: new Date() },
+    { id: 2, name: "Streak 2", longestStreak: 0, currentStreak: 0, lastUpdated: new Date() },
+    { id: 3, name: "Streak 3", longestStreak: 0, currentStreak: 0, lastUpdated: new Date() },
+    { id: 4, name: "Streak 4", longestStreak: 0, currentStreak: 0, lastUpdated: new Date() }
+])
